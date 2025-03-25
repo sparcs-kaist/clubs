7988d09a3e04d861501f178411cbba57f63b966d
edge <edge@sparcs.org>
2025-03-26 03:39:31 +0900

    feat: export excel download

--- packages/web/src/features/manage-club/members/frames/AllMemberListFrame.tsx
+++ packages/web/src/features/manage-club/members/frames/AllMemberListFrame.tsx
@@ -1,19 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import * as XLSX from "xlsx";
-
-import apiClb010, {
-  ApiClb010ResponseOk,
-} from "@sparcs-clubs/interface/api/club/endpoint/apiClb010";
 
 import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import IconButton from "@sparcs-clubs/web/common/components/Buttons/IconButton";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
 import Typography from "@sparcs-clubs/web/common/components/Typography";
-import { axiosClientWithAuth } from "@sparcs-clubs/web/lib/axios";
 
 import AllMemberList from "../components/AllMemberList";
 import MemberSearchAndFilter from "../components/MemberSearchAndFilter";
+import { useDownloadMembers } from "../hooks/useDownloadMembers";
 import { useGetClubSemesters } from "../services/useGetClubSemesters";
 import { SemesterProps } from "../types/semesterList";
 
@@ -35,9 +30,7 @@ const IconButtonWrapper = styled.div`
 `;
 
 const AllMemberListFrame: React.FC<AllMemberListFrameProps> = ({ clubId }) => {
-  const [searchText, setSearchText] = useState<string>("");
-  const [isDownloading, setIsDownloading] = useState<boolean>(false);
-
+  const [searchText, setSearchText] = useState("");
   const {
     data: semesterData,
     isLoading,
@@ -45,62 +38,32 @@ const AllMemberListFrame: React.FC<AllMemberListFrameProps> = ({ clubId }) => {
   } = useGetClubSemesters({ clubId });
 
   const [selectedSemesters, setSelectedSemesters] = useState<SemesterProps[]>(
-    semesterData.semesters,
+    [],
   );
 
-  const downloadExcel = async () => {
-    setIsDownloading(true);
-    try {
-      const allData = await Promise.all(
-        selectedSemesters.map(async semester => {
-          const response = await axiosClientWithAuth.get(
-            apiClb010.url(clubId, semester.id),
-          );
-          return { semester, members: response.data.members };
-        }),
-      );
+  const { isDownloading, downloadMembers } = useDownloadMembers();
 
-      const wb = XLSX.utils.book_new();
-
-      allData.forEach(({ semester, members }) => {
-        const sheetData = members.map(
-          (member: ApiClb010ResponseOk["members"][number]) => ({
-            학번: member.studentNumber,
-            신청자: member.name,
-            전화번호: member.phoneNumber,
-            이메일: member.email,
-          }),
-        );
-        const sheetName = `${semester.year}-${semester.name}`;
-        const ws = XLSX.utils.json_to_sheet(sheetData);
-        XLSX.utils.book_append_sheet(wb, ws, sheetName);
-      });
-
-      const wbout = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-      const blob = new Blob([wbout], { type: "application/octet-stream" });
-      const url = URL.createObjectURL(blob);
-      const downloadLink = document.createElement("a");
-      downloadLink.href = url;
-      downloadLink.download = "전체회원명단.xlsx";
-      downloadLink.click();
-      URL.revokeObjectURL(url);
-    } catch (error) {
-      console.error("엑셀 다운로드 실패", error);
+  useEffect(() => {
+    if (semesterData?.semesters) {
+      setSelectedSemesters(semesterData.semesters);
     }
-    setIsDownloading(false);
+  }, [semesterData]);
+
+  const handleDownload = () => {
+    downloadMembers(clubId, selectedSemesters);
   };
 
   return (
     <FoldableSectionTitle title="전체 회원 명단" childrenMargin="20px">
       <AsyncBoundary isLoading={isLoading} isError={isError}>
         <AllMemberListWrapper>
-          {semesterData.semesters.length > 0 && (
+          {semesterData?.semesters?.length > 0 && (
             <>
               <IconButtonWrapper>
                 <IconButton
                   type="default"
                   icon="save_alt"
-                  onClick={downloadExcel}
+                  onClick={handleDownload}
                 >
                   {isDownloading ? "다운로드 중..." : "엑셀로 다운로드"}
                 </IconButton>
