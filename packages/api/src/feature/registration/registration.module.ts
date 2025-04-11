100388045562db16860aa671454fce38d99232e3
Gerbera3090 <hw3090@gmail.com>
2025-04-11 16:22:10 +0900

    fix: merge club registration and member registraion modules to registration module

--- packages/api/src/feature/registration/registration.module.ts
+++ packages/api/src/feature/registration/registration.module.ts
@@ -1,15 +1,32 @@
-import { Module } from "@nestjs/common";
-import { DrizzleModule } from "src/drizzle/drizzle.module";
+import { forwardRef, Module } from "@nestjs/common";
 
-import { ClubRegistrationModule } from "./club-registration/club-registration.module";
+import { DrizzleModule } from "@sparcs-clubs/api/drizzle/drizzle.module";
+import ClubModule from "@sparcs-clubs/api/feature/club/club.module";
+import DivisionModule from "@sparcs-clubs/api/feature/division/division.module";
+import FileModule from "@sparcs-clubs/api/feature/file/file.module";
+import UserModule from "@sparcs-clubs/api/feature/user/user.module";
+
 import { RegistrationController } from "./controller/registration.controller";
-import { MemberRegistrationModule } from "./member-registration/member-registration.module";
-import { RegistrationRepository } from "./repository/registration.repository";
+import { ClubRegistrationRepository } from "./repository/club-registration.repository";
+import { MemberRegistrationRepository } from "./repository/member-registration.repository";
+import { RegistrationPublicService } from "./service/registration.public.service";
 import { RegistrationService } from "./service/registration.service";
 
 @Module({
-  imports: [DrizzleModule, MemberRegistrationModule, ClubRegistrationModule],
+  imports: [
+    DrizzleModule,
+    forwardRef(() => ClubModule),
+    DivisionModule,
+    FileModule,
+    UserModule,
+  ],
   controllers: [RegistrationController],
-  providers: [RegistrationRepository, RegistrationService],
+  providers: [
+    RegistrationService,
+    RegistrationPublicService,
+    ClubRegistrationRepository,
+    MemberRegistrationRepository,
+  ],
+  exports: [RegistrationPublicService],
 })
 export class RegistrationModule {}
