d052e9c6309151705ece7977e2833edc3363fb96
skykhs3 <skykhs3@naver.com>
2025-04-27 18:31:14 +0900

    feat: throw error when TEST_DATABASE_URL is not set

--- packages/api/src/env.ts
+++ packages/api/src/env.ts
@@ -5,7 +5,7 @@ const schema = z.object({
   SERVER_PORT: z.coerce.number(),
   SECRET_KEY: z.string(),
   DATABASE_URL: z.string(),
-  TEST_DATABASE_URL: z.string(),
+  TEST_DATABASE_URL: z.string().optional(),
 });
 
 const getSsoConfig = () => ({
@@ -15,6 +15,11 @@ const getSsoConfig = () => ({
 
 const env = schema.parse(process.env);
 if (env.NODE_ENV === "test") {
+  if (!env.TEST_DATABASE_URL) {
+    throw new Error(
+      "`TEST_DATABASE_URL` Environment Variable is not set. Please Set `TEST_DATABASE_URL` in `.env` file.",
+    );
+  }
   env.DATABASE_URL = env.TEST_DATABASE_URL;
 }
 
