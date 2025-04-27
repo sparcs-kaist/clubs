70b43a6adde5e451d397d58d4d3bfbc879c65e50
skykhs3 <skykhs3@naver.com>
2025-04-27 13:48:56 +0900

    feat: e2e test

--- packages/api/test/app.e2e-spec.ts
+++ packages/api/test/app.e2e-spec.ts
@@ -1,11 +1,13 @@
 import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
-import * as request from "supertest";
+import request from "supertest";
+import { App } from "supertest/types";
 
 import { AppModule } from "@sparcs-clubs/api/app.module";
+import { getConnection } from "@sparcs-clubs/api/drizzle/drizzle.provider";
 
 describe("AppController (e2e)", () => {
-  let app: INestApplication;
+  let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,8 +16,17 @@ describe("AppController (e2e)", () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-  });
+  }, 60000);
 
   it("/ (GET)", () =>
     request(app.getHttpServer()).get("/").expect(200).expect("Hello World!"));
+
+  afterAll(async () => {
+    // Close database connection
+    const connection = await getConnection();
+    await connection.end();
+
+    // Close the application
+    await app.close();
+  });
 });
