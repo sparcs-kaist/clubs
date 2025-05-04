1bab432f68e34dca06e139c41e8977946fb07bd0
YINGMEI <j.yeongmee@gmail.com>
2025-05-05 06:03:30 +0900

    fix: update activity duration date input min, max date to use activity deadline api

--- packages/web/src/features/register-club/components/activity-report/EditActivityTermModal.tsx
+++ packages/web/src/features/register-club/components/activity-report/EditActivityTermModal.tsx
@@ -1,6 +1,8 @@
+import { addDays, subDays } from "date-fns";
 import React, { useCallback, useMemo } from "react";
 import { Control, useFieldArray } from "react-hook-form";
 
+import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import IconButton from "@sparcs-clubs/web/common/components/Buttons/IconButton";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FormController from "@sparcs-clubs/web/common/components/FormController";
@@ -8,6 +10,7 @@ import DateInput from "@sparcs-clubs/web/common/components/Forms/DateInput";
 import Modal from "@sparcs-clubs/web/common/components/Modal";
 import CancellableModalContent from "@sparcs-clubs/web/common/components/Modal/CancellableModalContent";
 import Typography from "@sparcs-clubs/web/common/components/Typography";
+import useGetActivityDeadline from "@sparcs-clubs/web/features/activity-report/services/useGetActivityDeadline";
 import { ActivityReportFormData } from "@sparcs-clubs/web/features/activity-report/types/form";
 import { Duration } from "@sparcs-clubs/web/features/register-club/types/registerClub";
 import {
@@ -28,6 +31,8 @@ const EditActivityTermModal: React.FC<EditActivityTermModalProps> = ({
   onClose,
   onConfirm,
 }) => {
+  const { data: deadline, isLoading, isError } = useGetActivityDeadline();
+
   const { fields, append, remove, update } = useFieldArray<
     ActivityReportFormData,
     "durations"
@@ -72,89 +77,96 @@ const EditActivityTermModal: React.FC<EditActivityTermModalProps> = ({
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <CancellableModalContent onClose={onClose} onConfirm={handleConfirm}>
-        <FlexWrapper
-          direction="column"
-          gap={12}
-          style={{ width: "min(600px, 80vw)", height: "min(300px, 80vh)" }}
-        >
-          {fields.map((field, index) => (
-            <FlexWrapper
-              key={field.id}
-              direction="row"
-              gap={8}
-              style={{
-                width: "100%",
-                alignSelf: "center",
-                justifyContent: "center",
-              }}
-            >
-              <FormController
-                name={`durations.${index}`}
-                control={control}
-                renderItem={({ value, onChange, errorMessage }) => (
-                  <DateInput
-                    selectsRange
-                    minDate={new Date("2024-12-21")}
-                    maxDate={
-                      new Date() <= new Date("2025-06-13")
-                        ? new Date()
-                        : new Date("2025-06-13")
-                    }
-                    startDate={value?.startTerm}
-                    endDate={value?.endTerm}
-                    onChange={(dates: [Date, Date] | null) => {
-                      if (dates) {
-                        onChange({
-                          startTerm: dates[0],
-                          endTerm: dates[1],
-                        });
-                        update(index, {
-                          startTerm: dates[0],
-                          endTerm: dates[1],
-                        });
-                      }
-                    }}
-                    selectedDates={
-                      value?.startTerm && value?.endTerm
-                        ? [value.startTerm, value.endTerm]
-                        : null
-                    }
-                    placeholderText="20XX.XX.XX - 20XX.XX.XX"
-                    dateFormat="yyyy.MM.dd"
-                    errorMessage={errorMessage}
-                  />
-                )}
-              />
-              <IconButton
-                icon="delete"
-                onClick={() => handleDelete(index)}
-                style={{
-                  backgroundColor: "transparent",
-                  color: "black",
-                  alignSelf: "center",
-                }}
-              />
-            </FlexWrapper>
-          ))}
-          <IconButton
-            icon="add"
-            onClick={addRow}
-            style={{ backgroundColor: "white", color: "black" }}
-            type="outlined"
+        <AsyncBoundary isLoading={isLoading} isError={isError}>
+          <FlexWrapper
+            direction="column"
+            gap={12}
+            style={{ width: "min(600px, 80vw)", height: "min(300px, 80vh)" }}
           >
-            활동 기간 추가
-          </IconButton>
-          {isEmpty && (
-            <Typography fw="MEDIUM" fs={12} lh={18} color="RED.600">
-              기간을 하나 이상 추가해주세요.
-            </Typography>
-          )}
-          {isSomethingEmpty && (
-            <Typography fw="MEDIUM" fs={12} lh={18} color="RED.600">
-              기간을 입력하거나 해당 항목을 삭제해주세요.
-            </Typography>
-          )}
-        </FlexWrapper>
+            {fields.map((field, index) => (
+              <FlexWrapper
+                key={field.id}
+                direction="row"
+                gap={8}
+                style={{
+                  width: "100%",
+                  alignSelf: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <FormController
+                  name={`durations.${index}`}
+                  control={control}
+                  renderItem={({ value, onChange, errorMessage }) => (
+                    <DateInput
+                      selectsRange
+                      minDate={
+                        deadline?.targetTerm?.startTerm
+                          ? addDays(deadline.targetTerm.startTerm, 1)
+                          : undefined
+                      }
+                      maxDate={
+                        deadline?.targetTerm?.endTerm == null ||
+                        new Date() < deadline?.targetTerm?.endTerm
+                          ? new Date()
+                          : subDays(deadline.targetTerm.endTerm, 1)
+                      }
+                      startDate={value?.startTerm}
+                      endDate={value?.endTerm}
+                      onChange={(dates: [Date, Date] | null) => {
+                        if (dates) {
+                          onChange({
+                            startTerm: dates[0],
+                            endTerm: dates[1],
+                          });
+                          update(index, {
+                            startTerm: dates[0],
+                            endTerm: dates[1],
+                          });
+                        }
+                      }}
+                      selectedDates={
+                        value?.startTerm && value?.endTerm
+                          ? [value.startTerm, value.endTerm]
+                          : null
+                      }
+                      placeholderText="20XX.XX.XX - 20XX.XX.XX"
+                      dateFormat="yyyy.MM.dd"
+                      errorMessage={errorMessage}
+                    />
+                  )}
+                />
+                <IconButton
+                  icon="delete"
+                  onClick={() => handleDelete(index)}
+                  style={{
+                    backgroundColor: "transparent",
+                    color: "black",
+                    alignSelf: "center",
+                  }}
+                />
+              </FlexWrapper>
+            ))}
+            <IconButton
+              icon="add"
+              onClick={addRow}
+              style={{ backgroundColor: "white", color: "black" }}
+              type="outlined"
+            >
+              활동 기간 추가
+            </IconButton>
+            {isEmpty && (
+              <Typography fw="MEDIUM" fs={12} lh={18} color="RED.600">
+                기간을 하나 이상 추가해주세요.
+              </Typography>
+            )}
+            {isSomethingEmpty && (
+              <Typography fw="MEDIUM" fs={12} lh={18} color="RED.600">
+                기간을 입력하거나 해당 항목을 삭제해주세요.
+              </Typography>
+            )}
+          </FlexWrapper>
+        </AsyncBoundary>
       </CancellableModalContent>
     </Modal>
   );
