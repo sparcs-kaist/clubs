4e0ec69266219df8abc50ca8533d199dfec8f78c
Gerbera3090 <hw3090@gmail.com>
2025-05-05 04:15:32 +0900

    feat: division model, readmodel, repository

---
+++ packages/api/src/feature/division/model/old.division.model.ts
@@ -0,0 +1,34 @@
+import { InferSelectModel } from "drizzle-orm";
+
+import { IDivision } from "@clubs/interface/api/division/type/division.type";
+
+import { MEntity } from "@sparcs-clubs/api/common/base/entity.model";
+import { Division } from "@sparcs-clubs/api/drizzle/schema/division.schema";
+
+export type DivisionDBResult = InferSelectModel<typeof Division>;
+
+export class OldMDivision extends MEntity implements IDivision {
+  static modelName = "division";
+
+  name: IDivision["name"];
+
+  startTerm: IDivision["startTerm"];
+
+  endTerm: IDivision["endTerm"];
+
+  district: IDivision["district"];
+
+  constructor(data: IDivision) {
+    super();
+    Object.assign(this, data);
+  }
+
+  static from(result: DivisionDBResult): OldMDivision {
+    return new OldMDivision({
+      ...result,
+      district: {
+        id: result.districtId,
+      },
+    });
+  }
+}
--- packages/api/src/feature/division/service/division.public.service.ts
+++ packages/api/src/feature/division/service/division.public.service.ts
@@ -2,24 +2,48 @@ import { Injectable } from "@nestjs/common";
 
 import { getKSTDate } from "@sparcs-clubs/api/common/util/util";
 
-import DivisionRepository from "../repository/division.repository";
+import { RMDivision } from "../model/division.model";
+import { DistrictRepository } from "../repository/district.repository";
+import { DivisionRepository } from "../repository/division.repository";
+import OldDivisionRepository from "../repository/old.division.repository";
 
 @Injectable()
 export default class DivisionPublicService {
-  constructor(private readonly divisionRepository: DivisionRepository) {}
+  constructor(
+    private readonly oldDivisionRepository: OldDivisionRepository,
+    private readonly divisionRepository: DivisionRepository,
+    private readonly districtRepository: DistrictRepository,
+  ) {}
 
   async findDivisionById(id: number) {
-    const result = await this.divisionRepository.findDivisionById(id);
+    const result = await this.oldDivisionRepository.findDivisionById(id);
     return result;
   }
 
   async getDivisionById(param: { id: number }) {
-    const result = await this.divisionRepository.selectDivisionById(param);
+    const result = await this.oldDivisionRepository.selectDivisionById(param);
     return result;
   }
 
   async getCurrentDivisions() {
-    const result = await this.divisionRepository.fetchAll(getKSTDate());
+    const result = await this.oldDivisionRepository.fetchAll(getKSTDate());
     return result;
+  }
+
+  // 위 함수들은 다 old 임. 나중에 수정 필요
+  async search(query: { date?: Date }): Promise<Array<RMDivision>> {
+    const [divisions, districts] = await Promise.all([
+      this.divisionRepository.find(query),
+      this.districtRepository.find({}),
+    ]);
+
+    const districtMap = new Map(
+      districts.map(district => [district.id, district]),
+    );
+
+    return divisions.map(division => ({
+      ...division,
+      district: districtMap.get(division.district.id),
+    }));
   }
 }
