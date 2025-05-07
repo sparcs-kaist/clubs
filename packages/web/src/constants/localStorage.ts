0780812a0bceae86ebf38daeb259dc737f62edfc
edge <edge@sparcs.org>
2025-05-07 18:19:54 +0900

    feat: add loca store

--- packages/web/src/constants/localStorage.ts
+++ packages/web/src/constants/localStorage.ts
@@ -2,4 +2,5 @@ export const LOCAL_STORAGE_KEY = {
   ACTIVITY_REPORT: "activity-report",
   REGISTER_CLUB: "register-club",
   REGISTER_CLUB_ACTIVITY_REPORT_MODAL: "register-club-activity-report-modal",
+  EDIT_FUNDING: "edit-funding",
 } as const;
