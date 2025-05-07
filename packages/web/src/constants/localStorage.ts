22dc2be8cc05e3e205422286bfccbd37effd1430
edge <edge@sparcs.org>
2025-05-07 18:41:18 +0900

    feat: add local store create funding

--- packages/web/src/constants/localStorage.ts
+++ packages/web/src/constants/localStorage.ts
@@ -3,4 +3,5 @@ export const LOCAL_STORAGE_KEY = {
   REGISTER_CLUB: "register-club",
   REGISTER_CLUB_ACTIVITY_REPORT_MODAL: "register-club-activity-report-modal",
   EDIT_FUNDING: "edit-funding",
+  CREATE_FUNDING: "create-funding",
 } as const;
