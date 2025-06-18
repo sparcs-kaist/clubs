2592f886fb0c02a22827d885e065d341ac46f519
SeungJong Ha <hama@SeungJongs-Air.lan>
2025-06-18 22:24:01 +0900

    fix: remove student number checking on auth

--- packages/api/src/feature/auth/service/auth.service.ts
+++ packages/api/src/feature/auth/service/auth.service.ts
@@ -64,10 +64,12 @@ export class AuthService {
     logger.info(JSON.stringify(ssoProfile));
 
     const isKaistIamLogin: boolean = true;
-    if (process.env.NODE_ENV !== "local")
-      if (!ssoProfile.kaist_info.ku_std_no || !ssoProfile.sid) {
+    ssoProfile.kaist_info.ku_std_no = null;
+    if (process.env.NODE_ENV !== "local") {
+      if (!ssoProfile.sid || !ssoProfile.kaist_info) {
         return { isKaistIamLogin: false };
       }
+    }
 
     let studentNumber = ssoProfile.kaist_info.ku_std_no;
     let email = ssoProfile.kaist_info.mail?.replace("mailto:", "");
