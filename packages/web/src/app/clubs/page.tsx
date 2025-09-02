ba221431e879be7f3fa5ff13ca486a5d636efff1
YINGMEI ZHU <89931104+jooyeongmee@users.noreply.github.com>
2025-09-02 16:26:45 +0900

    [TU-299] 운영위원회 페이지 추가, 동아리 목록 배너 추가 (#1691)
    * fix: update turbo dev about cache
    
    * feat: add operating-committee funding page
    
    * fix: update operationg committee activity-report, funding pagehead
    
    * feat: add warning banner on club list page due to club-registration period
    
    * fix: update operating committee page head
    
    * fix: change acritivty report link
    
    ---------
    
    Co-authored-by: SeungJong Ha <hama@SeungJongs-MacBook-Air.local>

--- packages/web/src/app/clubs/page.tsx
+++ packages/web/src/app/clubs/page.tsx
@@ -3,11 +3,16 @@
 import React from "react";
 import styled from "styled-components";
 
+import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
+import Typography from "@sparcs-clubs/web/common/components/Typography";
 import { useAuth } from "@sparcs-clubs/web/common/providers/AuthContext";
 import ClubsListFrame from "@sparcs-clubs/web/features/clubs/frames/ClubsListFrame";
 import ClubsStudentFrame from "@sparcs-clubs/web/features/clubs/frames/ClubsStudentFrame";
+import useGetClubRegistrationDeadline from "@sparcs-clubs/web/features/clubs/services/useGetClubRegistrationDeadline";
+import Banner from "@sparcs-clubs/web/features/landing/components/Banner";
+import { formatDate } from "@sparcs-clubs/web/utils/Date/formatDate";
 import isStudent from "@sparcs-clubs/web/utils/isStudent";
 
 const ResponsiveWrapper = styled(FlexWrapper)`
@@ -22,12 +27,33 @@ const ResponsiveWrapper = styled(FlexWrapper)`
 const Clubs: React.FC = () => {
   const { isLoggedIn, profile } = useAuth();
 
+  const {
+    data,
+    isLoading: isLoadingDeadline,
+    isError: isErrorDeadline,
+  } = useGetClubRegistrationDeadline();
+
   return (
     <ResponsiveWrapper direction="column" gap={60}>
       <PageHead
         items={[{ name: "동아리 목록", path: "/clubs" }]}
         title="동아리 목록"
       />
+      <AsyncBoundary isLoading={isLoadingDeadline} isError={isErrorDeadline}>
+        {data?.deadline && (
+          <Banner icon="warning">
+            <Typography
+              fs={16}
+              lh={24}
+              color="GRAY.600"
+              style={{ whiteSpace: "pre-line" }}
+            >
+              {`"${formatDate(data?.deadline?.startDate)} - ${formatDate(data?.deadline?.endTerm)}"은 동아리 등록 기간입니다.
+              가을학기 동아리가 확정되지 않아 동아리 목록이 비어있어요. 동아리 대표자들께선 동아리 → 동아리 등록 기능을 통해 동아리 등록을 제출해주세요!`}
+            </Typography>
+          </Banner>
+        )}
+      </AsyncBoundary>
       {isLoggedIn && isStudent(profile) ? (
         <ClubsStudentFrame />
       ) : (
