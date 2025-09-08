b42c9c117133489c289fc05579edc05dda655b4b
Gerbera3090 <hw3090@gmail.com>
2025-09-08 21:19:03 +0900

    feat: gb credit

--- packages/web/src/features/credits/credits.ts
+++ packages/web/src/features/credits/credits.ts
@@ -51,6 +51,13 @@ const credits: SemesterCredit[] = [
         roleType: RoleType.member,
         comment: "졸업하자",
       },
+      {
+        nickname: "gb",
+        name: "권혁원",
+        role: "APM(BE)",
+        roleType: RoleType.APM_BE,
+        comment: "짜잔~ 내가 돌아왔다",
+      },
     ],
   },
   {
@@ -97,6 +104,13 @@ const credits: SemesterCredit[] = [
         role: "BE",
         roleType: RoleType.member,
         comment: "여름학기 수면 패턴 삭제 완료",
+      },
+      {
+        nickname: "gb",
+        name: "권혁원",
+        role: "APM(BE)",
+        roleType: RoleType.APM_BE,
+        comment: "저스트 두잇",
       },
     ],
   },
