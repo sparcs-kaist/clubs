b95a0c2e0c96832fa9ea0fffd333a8f854bca43b
Woojeong Choi <gldeel@kaist.ac.kr>
2025-09-12 02:14:58 +0900

    fix: 핸들러 추가

--- packages/web/src/features/my/register-club/frames/ProfessorRegisterClubDetailButton.tsx
+++ packages/web/src/features/my/register-club/frames/ProfessorRegisterClubDetailButton.tsx
@@ -3,15 +3,15 @@ import { useParams } from "next/navigation";
 import { overlay } from "overlay-kit";
 import React from "react";
 
-import apiReg011, {
-  ApiReg011ResponseOk,
-} from "@clubs/interface/api/registration/endpoint/apiReg011";
+import { ApiReg011ResponseOk } from "@clubs/interface/api/registration/endpoint/apiReg011";
+import apiReg022 from "@clubs/interface/api/registration/endpoint/apiReg022";
 
 import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import Button from "@sparcs-clubs/web/common/components/Button";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import Modal from "@sparcs-clubs/web/common/components/Modal";
 import CancellableModalContent from "@sparcs-clubs/web/common/components/Modal/CancellableModalContent";
+import { errorHandler } from "@sparcs-clubs/web/common/components/Modal/ErrorModal";
 import useGetClubRegistrationDeadline from "@sparcs-clubs/web/features/clubs/services/useGetClubRegistrationDeadline";
 import usePatchClubRegProfessorApprove from "@sparcs-clubs/web/features/my/services/usePatchClubRegProfessorApprove";
 
@@ -37,10 +37,15 @@ const ProfessorRegisterClubDetailButton: React.FC<{
             mutate(
               { param: { applyId: +id } },
               {
-                onSuccess: () => {
-                  queryClient.refetchQueries({
-                    queryKey: [apiReg011.url(String(id)), id],
+                onSuccess: async () => {
+                  await queryClient.refetchQueries({
+                    queryKey: [apiReg022.url(String(id)), id],
                   });
+                  close();
+                  errorHandler("승인이 완료되었습니다.");
+                },
+                onError: () => {
+                  errorHandler("승인에 실패하였습니다.");
                   close();
                 },
               },
