79b2f109df3ea6257d6efb61e16b7c5700a43cd5
권혁원 <52480724+Gerbera3090@users.noreply.github.com>
2026-06-02 01:02:47 +0900

    [TU-399] enforce single-condition MC/DC changes

--- packages/api/src/feature/activity/service/activity.service.new.ts
+++ packages/api/src/feature/activity/service/activity.service.new.ts
@@ -355,9 +355,12 @@ export default class ActivityService {
         // ActivityDeadlineEnum.Exception,
       ],
     });
-    const shouldResetProfessorApproval = availableDeadlines.some(
-      deadline => deadline.deadlineEnum === ActivityDeadlineEnum.Writing,
-    );
+    const shouldResetProfessorApproval = availableDeadlines.some(deadline => {
+      const deadlineEnum = deadline.deadlineEnum;
+      const writingDeadlineEnum = ActivityDeadlineEnum.Writing;
+
+      return deadlineEnum === writingDeadlineEnum;
+    });
     // 해당 활동이 지난 활동기간에 대한 활동인지 확인합니다.
     const activityD = await this.activityDurationPublicService.load();
     if (activity.activityDuration.id !== activityD.id)
